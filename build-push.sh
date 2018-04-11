b94219dad2f5931d78c0d10e98b713feb80456c6
shannon code <shannoncode@shannons-MacBook-Pro-2.local>
2018-04-11 17:01:07 -0400

    added ethereum addres libs

--- build-push.sh
+++ build-push.sh
@@ -1,5 +1,5 @@
-docker build . --tag us.gcr.io/marine-compass-175800/coval:v7 $1
-gcloud docker -- push us.gcr.io/marine-compass-175800/coval:v7
+docker build . --tag us.gcr.io/marine-compass-175800/coval:v9 $1
+gcloud docker -- push us.gcr.io/marine-compass-175800/coval:v9
 fission env delete --name covaljs
-fission env create --name covaljs --image us.gcr.io/marine-compass-175800/coval:v7
+fission env create --name covaljs --image us.gcr.io/marine-compass-175800/coval:v9
 bash env-vars.sh
